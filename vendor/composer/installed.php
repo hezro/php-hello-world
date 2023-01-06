a05e7afd41867c11461ce1de60b55df32d722fca
Orrin Rohrer <orrin.rohrer@gmail.com>
2023-01-06 16:33:22 -0600

    added symfony-yaml

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'efe01899d01d014b8ef6abd4fea85cc3e6ad40b2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'efe01899d01d014b8ef6abd4fea85cc3e6ad40b2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/yaml' => array(
+            'pretty_version' => 'v2.6.4',
+            'version' => '2.6.4.0',
+            'reference' => '60ed7751671113cf1ee7d7778e691642c2e9acd8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/yaml/Symfony/Component/Yaml',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
